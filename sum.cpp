c7777ff12c696073fa55ddc45a6d3d2824b98b9b
sruthemurugan <sruthemurugan08@gmail.com>
2024-11-12 10:40:29 +0530

    first commit

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+    int sum=0;
+    cout<<"Enter a number: ";
+    cin>>number;
+
+    while(number!=0){
+        sum+=number%10;
+        number/=10;
+    }
+    cout<<"Sum of digits is: "<<sum;
+    return 0;
+}
